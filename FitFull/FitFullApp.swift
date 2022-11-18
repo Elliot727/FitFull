55a248d13e4bafaa979b3cd9cd4928cda6ca80bd
Elliot727 <39097740+Elliot727@users.noreply.github.com>
2022-11-18 10:35:17 +0000

    Initial Commit

---
+++ FitFull/FitFullApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitFullApp.swift
+//  FitFull
+//
+//  Created by Elliot Silver on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct FitFullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
